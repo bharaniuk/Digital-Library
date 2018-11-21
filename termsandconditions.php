61e62a875b7a25648f44e4c8aa0f5cc26f09f055
divya883 <45230181+divya883@users.noreply.github.com>
2018-11-21 22:16:15 +0530

    Add files via upload

---
+++ termsandconditions.php
@@ -0,0 +1,43 @@
+<?php
+echo file_get_contents("header.html");
+?>
+  <div class="topPart" id="terms">
+    <h2>Terms and Conditions</h2>
+    <p>
+    We recommend that you carefully go through our Terms of Use, to better understand the different features of our service.
+    By subscribing to the registration process at BookHunters, Members accept all items in the Terms of Use and agree to abide by and be legally bound by the Terms of Use.
+    </p>
+    <p>
+      <b>1. Eligibility</b>
+      <ul>
+        <li>Anyone with an interest for reading can register.</li>
+        <li>Valid email id required</li>
+      </ul>
+      <b>2. Reading period and reading fee</b>
+      <ul>
+        <li>Every member can have a maximum of 1 book under their name at any given point of time</li>
+        <li>Late fee will be charged if books not returned within due date</li>
+        <li>A member can have a book for a maximum of 2 weeks after which they can reissue or return as required.</li>
+      </ul>
+      <b>3. Lost or damaged books</b>
+      <ul>
+      <li>At BookHunters, we take utmost care to maintain books in good condition.</li>
+      <li>Any anomaly (unprinted pages) of damage if found should be reported either at the front desk while getting the book issued</li>
+      <li>Failure to check and/or report the same will be constituted as if the member has received a book in good condition.</li>
+      </ul>
+      <b>4. Changes to Terms of Use</b>
+      <ul>
+        <li>BookHunters reserves the right, from time to time, with or without notice, to change these Terms of Use in its sole and absolute discretion.</li>
+        <li>Your continued use of our service after changes are made means that you agree to be bound by such changes.</li>
+      </ul>
+      <b>5. Data Security Policy</b>
+      <ul>
+        <li>Information provided by a member, while signing up to BookHunters services, such as identification and contact details, will be for the exclusive use of BookHunters. </li>
+        <li>Please read the conditions & stipulations set forth in this document, We look forward to your co-operation in adhering to the same.
+          Thank you for your membership. We will strive to provide you best-in-class reading experience at JustBooks.</li>
+      </ul>
+    </p>
+  </div>
+
+</body>
+</html>
